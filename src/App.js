5577664944869b72f8245eea7122fcc2516afd99
yoomajor <yoomajor@gmail.com>
2018-12-21 18:02:32 +0900

    inital settings

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">hi</div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
-import './index.scss';
+import './common.scss';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import { createStore } from 'redux'
-import reducers from './reducers';
-import { Provider } from 'react-redux';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// 스토어 생성
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
